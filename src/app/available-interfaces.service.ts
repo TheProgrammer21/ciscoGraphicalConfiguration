777e305078abf23fef24c9738ce4b5cd64e994d6
TheProgrammer21 <lukaswindisch21@gmail.com>
2019-07-03 20:07:43 +0200

    Implemented availableInterfaces

--- src/app/available-interfaces.service.ts
+++ src/app/available-interfaces.service.ts
@@ -5,5 +5,54 @@ import { Injectable } from '@angular/core';
 })
 export class AvailableInterfacesService {
 
-  constructor() { }
+  constructor() {
+    this.addInterfaceType("GigabitEthernet", "0/1-24");
+  }
+
+  private types: any = [{}];
+
+  public addInterfaceType(type: string, values: string) {
+    this.types.push({ type: type, range: values });
+  }
+
+  public getTypes(): string[] {
+    let result: string[] = []
+    this.types.forEach(e => result.push(e.type));
+    return result;
+  }
+
+  public getAllInterfacesOfType(type: string): string[] {
+    let values = this.types.find(e => e.type == type);
+    values = values.split("/");
+    let arr = []; //Beinhaltet alle aufgelösten Werte in Subarrays, jedes Subarray stellt einen von / getrennten Bereich dar
+
+    values.forEach(e => {
+      let subarr = []; //die Werte des aufzulösenden Bereiches, also alle Werte zwischen start-end
+      let boundaries = e.split("-");
+      let start: number = parseInt(boundaries[0]);
+      let end: number = boundaries.length == 0 ? start : parseInt(boundaries[1]);
+      for (; start <= end; start++) {
+        subarr.push(start);
+      }
+      arr.push(subarr);
+    });
+
+    values = this.concatParts(arr)[0];
+    values = values.map(e => type + " " + e);
+    return values;
+  }
+
+  private concatParts(result) { //verbindet Teile zu allen möglichen Kombinationen
+    while (result.length > 1) {
+      let res = [];
+      result[0].forEach(e1 => {
+        result[1].forEach((e2, n) => {
+          res.push(e1 + "/" + e2);
+        })
+      });
+      result.splice(0, 2);
+      result.unshift(res);
+    }
+    return result;
+  }
 }
